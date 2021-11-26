dff09bc63e093afa6edcd75a19452bf88728be9a
mackee <macopy123@gmail.com>
2021-11-26 18:36:54 +0900

    add tests

--- cpanfile
+++ cpanfile
@@ -3,10 +3,11 @@ requires 'SQL::Translator', '1.62';
 requires 'Furl';
 requires 'Try::Tiny';
 requires 'Moo';
+requires 'Type::Tiny';
 requires 'DBI';
 
 on 'test' => sub {
-    requires 'File::Path';
+    requires 'Test::Mock::Furl';
     requires 'Test::More', '0.98';
 };
 
--- lib/GitHubDDL.pm
+++ lib/GitHubDDL.pm
@@ -10,6 +10,8 @@ use SQL::Translator;
 use SQL::Translator::Diff;
 use DBI;
 use Furl;
+use Cwd;
+use Types::Standard qw/Str ArrayRef CodeRef/;
 use Try::Tiny;
 
 our $VERSION = "0.01";
@@ -18,34 +20,41 @@ use Moo;
 
 has ddl_file => (
     is       => 'ro',
-    isa      => 'Str',
+    isa      => Str,
     required => 1,
 );
 
 has dsn => (
     is       => 'ro',
-    isa      => 'ArrayRef[Str]',
+    isa      => ArrayRef,
     required => 1,
 );
 
 has ddl_version => (
     is       => 'ro',
-    isa      => 'Str',
+    isa      => Str,
     required => 1,
 );
 
 has version_table => (
     is      => 'ro',
-    isa     => 'Str',
+    isa     => Str,
     default => 'git_ddl_version',
 );
 
 has sql_filter => (
     is      => 'ro',
-    isa     => 'CodeRef',
+    isa     => CodeRef,
     default => sub {
         return sub { shift },
     },
+);
+
+has work_dir => (
+    is      => 'ro',
+    isa     => Str,
+    lazy    => 1,
+    default => sub { return Cwd::getcwd; },
 );
 
 has _dbh => (
@@ -56,19 +65,18 @@ has _dbh => (
 
 has github_user => (
     is       => 'ro',
-    isa      => 'Str',
+    isa      => Str,
     required => 1,
 );
 
 has github_repo => (
     is       => 'ro',
-    isa      => 'Str',
+    isa      => Str,
     required => 1,
 );
 
 has github_token => (
     is       => 'ro',
-    isa      => 'Str',
     required => 1,
 );
 
@@ -111,7 +119,7 @@ sub deploy {
     croak sprintf 'invalid version_table: %s', $self->version_table
         unless $self->version_table =~ /^[a-zA-Z_]+$/;
 
-    $self->_do_sql($self->_slurp(File::Spec->catfile($self->work_tree, $self->ddl_file)));
+    $self->_do_sql($self->_slurp(File::Spec->catfile($self->work_dir, $self->ddl_file)));
 
     $self->_do_sql(<<"__SQL__");
 CREATE TABLE @{[ $self->version_table ]} (
@@ -146,7 +154,7 @@ sub diff {
     $source->translate(\$source_sql) or croak $source->error;
 
     my $target_sql = $self->sql_filter->(
-        $self->_slurp(File::Spec->catfile($self->work_tree, $self->ddl_file))
+        $self->_slurp(File::Spec->catfile($self->work_dir, $self->ddl_file))
     );
     my $target = SQL::Translator->new;
     $target->parser($db) or croak $target->error;
@@ -223,8 +231,6 @@ sub _dump_sql_for_specified_commit {
     my ($self, $commit_hash, $outfile) = @_;
 
     open my $fh, '>', $outfile or croak $!;
-    print $fh $sql;
-    close $fh;
 
     my $url = sprintf "https://raw.githubusercontent.com/%s/%s/%s/%s",
         $self->github_user,
@@ -232,17 +238,18 @@ sub _dump_sql_for_specified_commit {
         $commit_hash,
         $self->ddl_file;
 
+    my $furl = Furl->new;
     my $res = $furl->request(
         method          => "GET",
         url             => $url,
         headers         => [
             Authorization => "token " . $self->github_token,
-            Accept        => "application/vnd.github.v3.raw",
+            Accept        => "application/vnd.github.v3+raw",
         ],
         write_code      => sub {
             my ( $status, $msg, $headers, $buf ) = @_;
             if ($status != 200) {
-                die "assetbundle status is not success: " . $filename;
+                die "status is not success when dump sql from GitHub: " . $self->ddl_file . ", status=" . $status;
             }
             print $fh $buf;
         }
@@ -290,6 +297,106 @@ GitHubDDL - GitDDL compatibility database migration utility when  hosted on GitH
 
 GitHubDDL is a tool module of the migration for RDBMS uses SQL::Translator::Diff.
 
+This is database migration helper module for users who manage database schema version by single .sql file in git repository.
+
+By using this module, you can deploy .sql to database, check sql version between database and .sql file, make diff between them, and apply alter table to database.
+
+=head1 METHODS
+
+=head2 GitHubDDL->new(%options)
+
+    my $gd = GitHubDDL->new(
+        ddl_file     => 'sql/schema_ddl.sql',
+        dsn          => ['dbi:mysql:my_project', 'root', ''],
+        ddl_version  => '...',
+        github_user  => '<your GitHub user/org name>',
+        github_repo  => '<your GitHub repository name>',
+        github_token => '<your GitHub token>',
+    );
+
+Create GitHubDDL object. Available options are:
+
+=over 4
+
+=item * ddl_file  => 'Str' (Required)
+
+ddl file (.sql file) path in repository.
+
+If ddl file located at /repos/project/sql/schema.sql and work_dir root is /repos/project, then this option should be sql/schema.sql
+
+=item * dsn => 'ArrayRef' (Required)
+
+DSN parameter that pass to L<DBI> module.
+
+=item * ddl_version => 'Str' (Required)
+
+DDL file's commit hash of local. If you need to apply schema to database by working dir's schema, you specify current commit hash of this file. B<THIS IS NOT OLDER COMMIT HASH>.
+
+=item * github_user => 'Str' (Required)
+
+GitHub's user or organization name of repository.
+
+=item * github_repo => 'Str' (Required)
+
+GitHub's repository name.
+
+=item * github_token => 'Str' (Required)
+
+GitHub's Personal Access Token. This is used to retrieve DDL that is applied to the database from GitHub.
+
+NOTE: If you need to use the authority of GitHub Apps, you can use L<GitHub::Apps::Auth> in it.
+
+=item * work_dir => 'Str' (Optional)
+
+Working directory of path includes target ddl file. Default is current working directory.
+
+=item * version_table => 'Str' (Optional)
+
+database table name that contains its git commit version. (default: git_ddl_version)
+
+=item * sql_filter => 'CodeRef' (Optional)
+
+CodeRef for filtering sql content. It is invoked only in C<< diff() >> method. (default: do nothing)
+
+=back
+
+=head2 check_version()
+
+Compare versions latest ddl sql and database, and return true when both version is same.
+
+Otherwise return false, which means database is not latest. To upgrade database to latest, see upgrade_database method described below.
+
+=head2 database_version()
+
+Return git commit hash indicates database's schema.
+
+=head2 ddl_version()
+
+Return git commit hash indicates ddl file.
+
+=head2 deploy()
+
+Just deploy ddl_file schema to database. This method is designed for initial database setup.
+But database should be created previously.
+
+=head2 diff()
+
+Show sql differences between ddl file and database.
+This method is useful for dry-run checking before upgrade_database.
+
+=head2 upgrade_database()
+
+Upgrade database schema to latest ddl schema.
+
+=head1 SEE ALSO
+=over 4
+=item *
+L<GitDDL>
+
+GitDDL is to compare local a DDL file and an older DDL from history in git.
+GitHubDDL is almost the same as GitDDL, but the only difference is that it retrieves the old DDL from GitHub.
+GitHubDDL uses a lot of code copied from GitDDL.
+
 =head1 LICENSE
 
 Copyright (C) mackee.
@@ -297,14 +404,8 @@ Copyright (C) mackee.
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
-=head1 SEE ALSO
-=over 4
-=item *
-L<GitDDL>
-
 =head1 AUTHOR
 
 mackee E<lt>macopy123@gmail.comE<gt>
 
 =cut
-
